5d0c2a8eefaa770d21c71693ea181c97a907eff1
McLutzifer <lukasschumi@gmx.at>
2021-03-13 15:55:30 +0100

    update

--- SOS/SOS.ino
+++ SOS/SOS.ino
@@ -1,29 +1,29 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // S.O.S. in morse code
   for (int x = 0; x < 3; x++) {
-    digitalWrite(LED_BUILTIN, HIGH);   
+    digitalWrite(13, HIGH);   
     delay(200);                      
-    digitalWrite(LED_BUILTIN, LOW);    
+    digitalWrite(13, LOW);    
     delay(200);       
   }
   for (int x = 0; x < 3; x++) {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(13, HIGH);
     delay(600);
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(13, LOW);
     delay(600);
   }
   for (int x = 0; x < 3; x++) {
-    digitalWrite(LED_BUILTIN, HIGH);   
+    digitalWrite(13, HIGH);   
     delay(200);                      
-    digitalWrite(LED_BUILTIN, LOW);    
+    digitalWrite(13, LOW);    
     delay(200);       
   }
 
-  digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(13,LOW);
   delay(1000);
 }
