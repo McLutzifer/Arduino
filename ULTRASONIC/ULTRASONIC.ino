5812b4c30b1c765719e8e881836c0f8c7fdcd638
McLutzifer <lukasschumi@gmx.at>
2021-03-14 12:19:43 +0100

    complete new

--- ULTRASONIC/ULTRASONIC.ino
+++ ULTRASONIC/ULTRASONIC.ino
@@ -1,19 +1,40 @@
-//www.elegoo.com
-//2016.12.08
-#include "SR04.h"
-#define TRIG_PIN 12
-#define ECHO_PIN 11
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long a;
+// This uses Serial Monitor to display Range Finder distance readings
 
-void setup() {
-   Serial.begin(9600);
-   delay(1000);
+// Include NewPing Library
+#include "NewPing.h"
+
+// Hook up HC-SR04 with Trig to Arduino Pin 9, Echo to Arduino pin 10
+#define TRIGGER_PIN 9
+#define ECHO_PIN 10
+
+// Maximum distance we want to ping for (in centimeters).
+#define MAX_DISTANCE 400  
+
+// NewPing setup of pins and maximum distance.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+float duration, distance;
+
+void setup() 
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-   a=sr04.Distance();
-   Serial.print(a);
-   Serial.println("cm");
-   delay(1000);
+void loop() 
+{
+  // Send ping, get distance in cm
+  distance = sonar.ping_cm();
+  
+  // Send results to Serial Monitor
+  Serial.print("Distance = ");
+  
+  if (distance >= 400 || distance <= 2) 
+  {
+    Serial.println("Out of range");
+  }
+  else 
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
 }
