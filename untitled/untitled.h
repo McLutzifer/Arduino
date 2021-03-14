c878c71bb5033e229cbdfb702e2c2fb1d18192e0
McLutzifer <lukasschumi@gmx.at>
2021-03-14 17:34:22 +0100

    CLion Test

---
+++ untitled/untitled.h
@@ -0,0 +1,10 @@
+#ifndef _UNTITLED_H_
+#define _UNTITLED_H_
+
+class Untitled {
+
+public:
+
+};
+
+#endif //_UNTITLED_H_
---
+++ untitled/untitled_test.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include "untitled.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
