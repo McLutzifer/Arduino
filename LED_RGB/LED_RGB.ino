b9caa0b682a82e43810191bd6026b3f77126cf7c
McLutzifer <lukasschumi@gmx.at>
2021-03-14 11:18:00 +0100

    add green

--- LED_RGB/LED_RGB.ino
+++ LED_RGB/LED_RGB.ino
@@ -13,6 +13,34 @@ void setup() {
   digitalWrite(BLUE, LOW);
 }
 
+//define variables
+int redValue;
+int greenValue;
+int blueValue;
+
+
+// main loop
 void loop() {
+  #define delayTime 10 // fading time between colors
+
+  redValue = 255; // choose a value between 1 and 255 to change the color
+  greenValue = 0;
+  blueValue = 0;
+
+
+  for (int i = 0; i < 255; i++)
+  {
+    redValue --;
+    greenValue++;
  
+    analogWrite(RED, redValue);
+    analogWrite(GREEN, greenValue);
+    delay(delayTime)
+  }
+
+  redValue = 0;
+  greenValue = 255;
+  blueValue = 0;
+
+  for (int i = 0
 }
