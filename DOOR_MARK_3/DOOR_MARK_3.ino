0f0ede9314cf3cf57d9ca666e6b7b4fb90b790ca
McLutzifer <lukasschumi@gmx.at>
2021-03-23 14:38:29 +0100

    create Door Mk III

---
+++ DOOR_MARK_3/DOOR_MARK_3.ino
@@ -0,0 +1,67 @@
+
+
+
+// DEF for LED chain
+
+int tDelay = 100;
+int latchPin = 15;      // (11) ST_CP [RCK] on 74HC595
+int clockPin = 14;      // (9) SH_CP [SCK] on 74HC595
+int dataPin = 16;     // (12) DS [S1] on 74HC595
+
+byte leds = 0;
+
+
+// PROTOTYPES //////////////////////////////////////
+void updateShiftRegister();
+void ledlight(int);
+
+
+
+
+
+/////////// SETUP /////////////////////////////////
+
+
+void setup() 
+{
+	/* LED chain*/
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+}
+
+
+
+
+////////////LOOP////////////////////////
+
+void loop() 
+{
+  leds = 0;
+  updateShiftRegister();
+  delay(tDelay);
+
+  for (int i = 8; i > 0; i--) {
+    ledlight(i);
+  }
+}
+
+
+
+// FUNCTIONS FOR LED chain
+
+void updateShiftRegister()
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
+
+void ledlight(int port) 
+{
+    bitSet(leds, port);
+    updateShiftRegister();
+    delay(tDelay);
+}
+
+
