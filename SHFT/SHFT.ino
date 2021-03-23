5809dca9b43ab4767ee587f8f7c55bfd2cc18a86
McLutzifer <lukasschumi@gmx.at>
2021-03-23 19:26:43 +0100

    update

--- SHFT/SHFT.ino
+++ SHFT/SHFT.ino
@@ -1,6 +1,28 @@
+int DS_pin = 16;
+int STCP_pin = 15;
+int SHCP_pin = 14;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(DS_pin, OUTPUT);
+  pinMode(STCP_pin, OUTPUT);
+  pinMode(SHCP_pin, OUTPUT);writereg();
+ 
 
+}
+
+boolean regsters(8);
+
+void writereg()
+{
+  digitalWrite(SHCP_pin,LOW);
+
+  for (int i = 7; i >= 0; i--)
+  {
+    digitalWrite(STCP_pin,LOW);
+    digitalWrite(DS_pin, registers(i));
+    digitalWrite(STCP_pin, HIGH);
+  }
 }
 
 void loop() {
