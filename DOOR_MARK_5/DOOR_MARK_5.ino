2b1ce0dc125c3a40511af78d4e56912561d49cb7
McLutzifer <lukasschumi@gmx.at>
2021-03-23 20:13:18 +0100

    creating Door Mk 5

---
+++ DOOR_MARK_5/DOOR_MARK_5.ino
@@ -0,0 +1,95 @@
+
+
+//////////////////////////////////
+#include <EEPROM.h>     // We are going to read and write PICC's UIDs from/to EEPROM
+#include <SPI.h>        // RC522 Module uses SPI protocol
+#include <MFRC522.h>  // Library for Mifare RC522 Devices
+
+
+
+
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+#define DS_pin 16
+#define STCP_pin 15
+#define SHCP_pin 14
+
+
+boolean registers[8];  //default value = false = 0
+
+int delayTime = 100;
+
+
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+
+
+//////////////////////////////////////////////////////
+
+  pinMode(DS_pin, OUTPUT);
+  pinMode(STCP_pin, OUTPUT);
+  pinMode(SHCP_pin, OUTPUT);
+  writeReg();
+ 
+
+}
+
+
+void writeReg()
+{
+  digitalWrite(STCP_pin,LOW);
+
+  for (int i = 7; i >= 0; i--)
+  {
+    digitalWrite(SHCP_pin,LOW);
+    digitalWrite(DS_pin, registers[i]);
+    digitalWrite(SHCP_pin, HIGH);
+  }
+  digitalWrite(STCP_pin, HIGH);
+}
+
+
+
+void loop() {
+	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+	//mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+  if ( EEPROM.read(0))
+  {
+      for (int i = 0; i <8; i++)
+  {
+    registers[i] = HIGH;
+    delay(delayTime);
+    writeReg();
+  }
+  for (int i = 7; i >= 0; i--)
+  {
+    registers[i] = LOW;
+    delay(delayTime);
+    writeReg();
+  }
+
+  }
+}
