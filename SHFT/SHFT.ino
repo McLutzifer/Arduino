a53eaa239619ccd0b99a7f89e3c537c88d2b822f
McLutzifer <lukasschumi@gmx.at>
2021-03-23 20:12:45 +0100

    colourshift

--- SHFT/SHFT.ino
+++ SHFT/SHFT.ino
@@ -1,47 +1,51 @@
-int DS_pin = 16;
-int STCP_pin = 15;
-int SHCP_pin = 14;
+#define DS_pin 16
+#define STCP_pin 15
+#define SHCP_pin 14
 
-//better     #define STCP_pin 9
+
+boolean registers[8];  //default value = false = 0
+
+int delayTime = 100;
 
 void setup() 
 {
   pinMode(DS_pin, OUTPUT);
   pinMode(STCP_pin, OUTPUT);
   pinMode(SHCP_pin, OUTPUT);
-  writereg();
+  writeReg();
  
 
 }
 
-boolean registers[8];
 
-void writereg()
+void writeReg()
 {
-  digitalWrite(SHCP_pin,LOW);
+  digitalWrite(STCP_pin,LOW);
 
   for (int i = 7; i >= 0; i--)
   {
-    digitalWrite(STCP_pin,LOW);
+    digitalWrite(SHCP_pin,LOW);
     digitalWrite(DS_pin, registers[i]);
-    digitalWrite(STCP_pin, HIGH);
+    digitalWrite(SHCP_pin, HIGH);
   }
-  digitalWrite(SHCP_pin, HIGH);
+  digitalWrite(STCP_pin, HIGH);
 }
 
 void loop() 
 {
-  for (int i = 0; i <9; i++)
+  for (int i = 0; i <8; i++)
   {
     registers[i] = HIGH;
-    delay(100);
-    writereg();
+    delay(delayTime);
+    writeReg();
   }
-  for (int i = 8; i > 0; i--)
+  for (int i = 7; i >= 0; i--)
   {
     registers[i] = LOW;
-    delay(100);
-    writereg();
+    delay(delayTime);
+    writeReg();
   }
+
+  
 
 }
